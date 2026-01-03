f62b7421a0c4c56bde8722ca349a801e542baeea
Tianyi Wu <tianyiwu@mac.lan>
2026-01-03 09:30:10 -0800

    update vite config with base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/global-counter-app/', // <--- ADD THIS LINE (use your exact repo name)
 })
